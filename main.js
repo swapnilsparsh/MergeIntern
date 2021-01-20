bb28d9f2f4200c3c383fa07965e93b442f1f729e
swapnilsparsh <arezona.lucky55@gmail.com>
2021-01-20 17:31:41 +0530

    Frontend Track

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('nav a [href*="#"]').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
+    });
+
+    $('#up').on('click', function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+    });
+});
